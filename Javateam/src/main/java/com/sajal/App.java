24af292b6969acb770ff218d5e2b44e31942427a
Sajal <sajal.kumar2003@gmail.com>
2025-05-28 11:00:42 +0530

    add maven

---
+++ Javateam/src/main/java/com/sajal/App.java
@@ -0,0 +1,13 @@
+package com.sajal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
